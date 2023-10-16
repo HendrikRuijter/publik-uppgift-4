c519423e144517364cce2bc08d5fe0ea7ee35137
Hendrik Ruijter <hendrik.ruijter@gmail.com>
2023-10-16 15:00:58 +0200

    Initial Commit

---
+++ uppgift-4/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  uppgift-4
+//
+//  Created by Hendrik on 2023-10-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ uppgift-4/uppgift_4App.swift
@@ -0,0 +1,17 @@
+//
+//  uppgift_4App.swift
+//  uppgift-4
+//
+//  Created by Hendrik on 2023-10-16.
+//
+
+import SwiftUI
+
+@main
+struct uppgift_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
